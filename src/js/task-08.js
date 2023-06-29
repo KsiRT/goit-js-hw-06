b9510b042f1a48ba5d0b73c5d0d67ede78b217e8
KsiRT <130671776+KsiRT@users.noreply.github.com>
2023-06-29 23:07:17 +0300

    t 8&9

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,29 @@
+const formEl = document.querySelector(".login-form");
 
+const logInputEls = formEl.getElementsByTagName("input");
+//* массив из двух инпутов
+const emailInp = logInputEls[0];
+const passInp = logInputEls[1];
+const onSubmitValidation = (event) => {
+  event.preventDefault();
+  console.log("Submitted");
+  const emailLength = emailInp.value.length;
+  const passLength = passInp.value.length;
+  if (emailLength === 0 || passLength === 0) {
+    return alert("All fields must be filled :P");
+  }
+
+  const formSubmissionResult = {
+    Email: emailInp.value,
+    Password: passInp.value,
+  };
+  event.target.reset();
+  console.log(formSubmissionResult);
+  return formSubmissionResult;
+};
+
+formEl.addEventListener("submit", onSubmitValidation);
+
+// ? Вопрос по 4 пункту приема задания "Для доступу до елементів форми використовуй властивість elements" что имеется ввиду?
+
+// Електрички очень старался убирать :D
--- src/js/task-09.js
+++ src/js/task-09.js
@@ -3,3 +3,14 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+const colorChangeBtn = document.querySelector(".change-color");
+const bodyEl = document.querySelector("body");
+const colorSpan = document.querySelector(".color");
+
+const onColorChange = () => {
+  let randomColor = getRandomHexColor();
+  bodyEl.style.backgroundColor = randomColor;
+  colorSpan.textContent = randomColor;
+  console.dir(randomColor);
+};
+colorChangeBtn.addEventListener("click", onColorChange);
