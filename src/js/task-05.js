4b480742413c2163d308bcb11cfdb181b29fece1
KsiRT <130671776+KsiRT@users.noreply.github.com>
2023-06-29 16:19:15 +0300

    t5

--- src/js/task-05.js
+++ src/js/task-05.js
@@ -1,1 +1,10 @@
-const nameInput = document.querySelector("#name-input");
+const nameInputEl = document.querySelector("#name-input");
+const nameOutputEl = document.querySelector("#name-output");
+
+nameInputEl.addEventListener("input", (event) => {
+  if (event.target.value === "") {
+    nameOutputEl.textContent = "Anonymous";
+    return;
+  }
+  nameOutputEl.textContent = event.target.value;
+});
