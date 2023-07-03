ecae86381e546cc46347b3788751a10dd262343c
KsiRT <130671776+KsiRT@users.noreply.github.com>
2023-07-03 12:53:50 +0300

    update 6 task

--- js/task-06.js
+++ js/task-06.js
@@ -1,16 +1,20 @@
 const validatingInputEl = document.querySelector("#validation-input");
-console.dir(validatingInputEl);
-const requiredLengthData = validatingInputEl.dataset;
+// console.dir(validatingInputEl);
+// const requiredLengthData = validatingInputEl.dataset;
 // console.dir(requiredLengthData);
-const { length } = requiredLengthData;
+// const { length } = requiredLengthData;
 // console.dir(length);
 validatingInputEl.addEventListener("blur", (evt) => {
-  let lengthOfInput = Number(evt.target.value.length);
+  const length = evt.target.dataset.length;
+  const targetClass = evt.target.classList;
+  const lengthOfInput = Number(evt.target.value.length);
+
   if (lengthOfInput === Number(length)) {
-    evt.target.classList.remove("invalid");
-    evt.target.classList.add("valid");
+    targetClass.remove("invalid");
+    targetClass.add("valid");
   } else {
-    evt.target.classList.remove("valid");
-    evt.target.classList.add("invalid");
+    targetClass.remove("valid");
+    targetClass.add("invalid");
   }
+  console.dir(evt.target);
 });
